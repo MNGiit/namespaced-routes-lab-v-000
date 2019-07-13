4438353afc582f3b84185494a47d526cafce5fd6
Mohammed Noori <kmanoorib@gmail.com>
2019-07-13 01:39:58 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,5 +1,5 @@
 class Admin::PreferencesController < ApplicationController
   def index
-    @preference = Preference.fi
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
 end

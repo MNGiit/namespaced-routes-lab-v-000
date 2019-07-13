1bee4191fd74c108eb305c848f7a0cdb5a6c4f71
Mohammed Noori <kmanoorib@gmail.com>
2019-07-13 01:32:23 +0000

    Automatically backed up by Learn

--- db/migrate/20190713013128_create_preferences.rb
+++ db/migrate/20190713013128_create_preferences.rb
@@ -1,4 +1,5 @@
 class CreatePreferences < ActiveRecord::Migration[5.0]
+  # rails g migration create_preferences allow_create_artists:boolean allow_create_songs:boolean artist_sort_order:string song_sort_order:string --no-test-framework
   def change
     create_table :preferences do |t|
       t.boolean :allow_create_artists
